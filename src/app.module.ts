362b7a0f7b616bf9755f8b2dcfca94e4dfac7195
yeonwoochoi <chldusdn20@gmail.com>
2025-02-09 22:50:33 +0900

    feat: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { BoardsModule } from './boards/boards.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { TestModule } from './test/test.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { TestModule } from './test/test.module';
     }),
     DatabaseModule,
     BoardsModule,
+    AuthModule,
     // TestModule
   ]
 })
